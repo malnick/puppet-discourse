3353cd50549a66afc8343b12c62e625b6da193ea
malnick <malnick@gmail..com>
2015-08-05 11:45:08 -0700

    config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,4 +1,5 @@
 class discourse::config (
+  $mandril_api_key,
   $discourse_root             = $discourse::params::discourse_root,
   $pup_templates              = $discourse::params::pup_templates,
   $pup_templates_module       = $discourse::params::pup_templates_module,
