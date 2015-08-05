9228a56b6f4370a165b4b215ed08463908969758
malnick <malnick@gmail..com>
2015-08-05 10:20:24 -0700

    discourse service

--- manifests/service.pp
+++ manifests/service.pp
@@ -4,23 +4,17 @@ class discourse::service (
 
 ) inherits discourse::params {
 
-    service { 'discourse':
-        ensure     => running,
-        enable     => true,
-        start      => "${discourse_root}/./launcher start app",
-        stop       => "${discourse_root}/./launcher stop app",
-        hasrestart => true,
-        pattern    => 'discourse',
-        provider   => 'base',
-        require    => Exec['bootstrap_discourse'],
-    }
 
-    exec { 'bootstrap_discourse':
-        cwd         => "${discourse_root}",
-        command     => "${discourse_root}/./launcher bootstrap app < y",
-        refreshonly => true,
-        timeout     => '0',
-        logoutput   => true,
-        refreshonly => true,
-    }
+  file { '/etc/init.d/discourse':
+    ensure      => file,
+    mode        => '0755',
+    source      => 'puppet:///modules/discourse/discourse_init.erb',
+    notify      => Service['discourse'],
+  }
+
+  service { 'discourse':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+  }
 }
