1a2903e67de2348a596f642420bb009f1f5a4229
malnick <malnick@gmail..com>
2015-08-05 11:20:48 -0700

    manifests/params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,7 +7,7 @@ class discourse::params {
     $ssl_cert                   = undef
     $manage_docker              = true
     $docker_bind                = '0.0.0.0'
-    $discourse_project_repo     = 'https://github.com/discourse/discourse_docker.git', 
+    $discourse_project_repo     = 'https://github.com/discourse/discourse_docker.git'
     $pup_templates              = ['web_only.yml']
     $pup_templates_module       = 'discourse'
 }
