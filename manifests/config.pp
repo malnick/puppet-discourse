27c220d668a8d20909d52d262dea89c254e2fd7c
malnick <malnick@gmail..com>
2015-08-10 09:03:59 -0700

    commit code

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,4 +1,5 @@
 class discourse::config (
+  $main_template              = undef,
   $mandril_api_key            = undef,
   $discourse_psql_pw          = undef,
   $discourse_root             = $discourse::params::discourse_root,
@@ -7,6 +8,14 @@ class discourse::config (
   $ssl_key                    = $discourse::params::ssl_key, 
   $ssl_cert                   = $discourse::params::ssl_cert,
 ) inherits discourse::params {
+
+  $main_base = basename($main_template, '.erb')
+
+  file {$main_template:
+    ensure    => file,
+    path      => "${discourse_root}/templates/${main_base}",
+    content   => template("${pup_templates_module}/${main_template}"),
+  }
 
   discourse::pup_template { $pup_templates:
     pup_templates_module  => $pup_templates_module,
