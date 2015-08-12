797726acc60873c15ef31108a8c7a637d371e387
malnick <malnick@gmail..com>
2015-08-12 11:02:08 -0700

    pup_template.pp

--- manifests/pup_template.pp
+++ manifests/pup_template.pp
@@ -5,6 +5,7 @@ define discourse::pup_template (
   $psql_password,
 ){
   $temp_base = basename($template, '.erb')
+  notify {"${temp_base}: ${psql_password}":}
   file { $template:
     ensure    => file,
     path      => "${discourse_root}/templates/${temp_base}",
